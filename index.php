c9867a7e2b9581bd4e9b077012b9528bb623a45d
Abraham <abrahamgraciosa@gmail.com>
2024-11-19 08:46:22 +0800

    Added read functionality to display tasks

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include 'db.php';
+
+$result = $conn->query("SELECT * FROM tasks ORDER BY created_at DESC");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +13,37 @@
 </head>
 <body>
   <div class="container">
-    <h1>To-Do List</h1>
+    <h1>Josh Todo_List</h1>
 
+    
     <form action="add_task.php" method="POST">
         <input type="text" name="title" placeholder="Task Title" required>
         <textarea name="description" placeholder="Task Description"></textarea>
         <button type="submit">Add Task</button>
     </form>
+
     
-   </div>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Actions</th>
+        </tr>
+   <?php while($task = $result->fetch_assoc()) { ?>
+            <tr>
+                <td><?php echo $task['id']; ?></td>
+                <td><?php echo $task['title']; ?></td>
+                <td><?php echo $task['description']; ?></td>
+                <td><?php echo $task['status']; ?></td>
+                <td>
+                    <a href="edit_task.php?id=<?php echo $task['id']; ?>">Edit</a>
+                    <a href="delete_task.php?id=<?php echo $task['id']; ?>" onclick="return confirm('Are you sure?')">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+    </div>
 </body>
 </html>
